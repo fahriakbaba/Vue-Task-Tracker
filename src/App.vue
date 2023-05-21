1c25e4a558c2a920d5b2d8bdbad5941117cb0742
Fahri Akbaba <fahriakbaba@gmail.com>
2023-05-21 10:48:32 +0300

    Header component is created

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+  },
+  data() {
+    return {
+      todos: [],
+    }
+  },
+  methods: {
+    deleteMessage(msg) {
+      console.log(msg);
+      console.log("todos: ", this.todos);
+    }
+  },
+  async mounted() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await res.json();
+    this.todos = data; 
+  }
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <h3>Task Tracker</h3>
+        <button class="btn">button</button>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    
+}
+</script>
+
+<style>
+</style>

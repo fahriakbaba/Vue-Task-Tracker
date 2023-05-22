072df73ade4712f6c5b8de34f3cceb014b7516c5
Fahri Akbaba <fahriakbaba@gmail.com>
2023-05-22 19:24:11 +0300

    html content in AddTask component is designed

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,7 +1,19 @@
 <template>
-    <section>
-        add task context
-    </section>
+    <form class="form">
+        <div class="form-group">
+            <label for="task">Task</label>
+            <input id="task" type="text" placeholder="Add Task" />
+        </div>
+        <div class="form-group">
+            <label for="dayAndTime">Day & Time</label>
+            <input id="dayAndTask" type="text" placeholder="Add Day & Time" />
+        </div>
+        <div class="form-reminder">
+            <label for="reminder">Set Reminder</label>
+            <input type="checkbox" name="reminder" id="reminder">
+        </div>
+        <button type="submit">Save Task</button>
+    </form>
 </template>
 
 <script>
@@ -11,5 +23,18 @@ export default {
 </script>
 
 <style scoped>
+.form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: .2rem;
+    margin-bottom: .75rem;
+}
 
+.form-group {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+}
 </style>

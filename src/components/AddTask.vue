4ac6ab4139520df0577d4759a9566592f8c338d5
Fahri Akbaba <fahriakbaba@gmail.com>
2023-05-22 23:17:01 +0300

    AddTask component is styled by using CSS

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -36,5 +36,48 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: flex-start;
+    margin-top: .75rem;
+}
+
+.form-group label {
+    font-size: .85rem;
+    font-weight: 600; 
+    margin-bottom: .25rem;  
+    letter-spacing: .5px;
+}
+
+.form-group input {
+    width: 100%;
+    border-radius: 3px;
+    padding: .3rem .6rem;
+    transition: border .3s;
+    outline: none;
+    border: 2px solid gray;
+}
+
+.form-group input:focus {
+    border: 2px solid rgba(28, 28, 214, 0.774);
+}
+
+.form-reminder {
+    display: flex;
+    gap: 7rem;
+    align-items: center;
+    margin-top: .75rem;
+    font-size: .85rem;
+    font-weight: 600; 
+}
+
+.form-reminder input {
+    cursor: pointer;
+}
+
+button {
+    margin-top: .75rem;
+    padding: .3rem .6rem;
+    background-color: black;
+    color: white;
+    border-radius: 3px;
+    cursor: pointer;
 }
 </style>
